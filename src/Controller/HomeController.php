6902ba4bc508c7ae893c3d4ee4b23dc999311bc2
Pauline <pauline.frare-pradal@laposte.net>
2022-05-27 16:15:21 +0200

    ci: fetch test local  content

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Hebergement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,6 +21,14 @@ class HomeController extends AbstractController
      */
     public function hebergements(): Response
     {
+        // Crée manuellement le contenu
+        $hebergement = new Hebergement();
+        $hebergement->setTitle('Logement 1')
+                    ->setDescription('Fugiat voluptate sit ea est aute tempor qui et nisi minim veniam. ');
+
+        dd($hebergement);
+
+
         return $this->render('home/tout-nos-hebergements.html.twig');
     }
     
