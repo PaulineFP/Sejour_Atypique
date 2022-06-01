3aad8e1d87ef45e6554362e7d0d16409bdd426c4
Pauline <pauline.frare-pradal@laposte.net>
2022-06-01 19:16:36 +0200

    Ci: resolve bug fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,29 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Hebergement;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 Use Faker\factory;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
-        $faker = Faker\Factory::create(fr_FR);
+        $faker = Factory::create('fr_FR');
 
         for ($i=0; $i<10; $i++) {
         // Crée manuellement un contenu fictif
         $hebergement = new Hebergement();
         $hebergement->setTitle($faker->sentence($nbWords = 2, $variableNbWords = true))
                     ->setDescription($faker->sentence($nbWords = 10, $variableNbWords = true))
-                    ->setTarif('150')
+                    ->setTarif($faker->randomNumber(3))
                     ->setLieux('ICI');
 
          $manager->persist($hebergement);
         }
-        ]
+        
         $manager->flush();
     }
 }
