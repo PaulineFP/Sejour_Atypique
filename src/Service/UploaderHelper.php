306120cbcb79c67eca100b9c145e0445a214fc9c
Pauline <pauline.frare-pradal@laposte.net>
2022-09-30 09:36:55 +0200

    :computer: Ci : Using the RequestStackContext

--- src/Service/UploaderHelper.php
+++ src/Service/UploaderHelper.php
@@ -1,18 +1,30 @@
 <?php
 namespace App\Service;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-
+use Symfony\Component\Asset\Context\RequestStackContext;
 
 class UploaderHelper
 {
+    private $uploadsPath;
+    
     const HEBERGEMENT_IMAGE = 'images/hebergements';
+
+    public function __construct(string $uploadsPath, RequestStackContext $requestStackContext)
+    {
+        $this->uploadsPath = $uploadsPath;
+        $this->requestStackContext = $requestStackContext;
+    }
 
     public function uploadHebergementImage(UploadedFile $uploadedFile){
         $destination = $this->uploadsPath.'/'.self::HEBERGEMENT_IMAGE;
     }
 
     public function getPublicPath(string $path): string {
-        return 'upload/'.$path;
-    }
+        // return 'upload/'.$path;
+        // nécessaire si vous déployez sous un sous-répertoire
+        return $this->requestStackContext
+            ->getBasePath().'/upload/'.$path;
+    } 
+   
 }
 ?>
