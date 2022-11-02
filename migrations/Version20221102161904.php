68d26026f61a868c8fbc70485f69904226887bf7
Pauline <pauline.frare-pradal@laposte.net>
2022-11-02 17:24:00 +0100

    :computer: Ci : add media/equipment/pecularity on Hebergement page

---
+++ migrations/Version20221102161904.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221102161904 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE media CHANGE alt media_alt VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE media CHANGE media_alt alt VARCHAR(255) DEFAULT NULL');
+    }
+}
