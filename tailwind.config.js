598bcffd30ff457bd47e0f51d600c8ddddf743c5
Pauline <pauline.frare-pradal@laposte.net>
2023-02-09 11:22:37 +0100

    :sparkles: Style: add filter and truncate for hebergement promotion  presentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
         colors: {
           'text-cyan-700': 'rgb(14, 116, 144)',
           'text-cyan-800'	: 'rgb(21, 94, 117)',
-          'text-teal-600'	: '#0f120a'
+          'text-teal-600'	: '#0f120a',
+          // couleur palette
+          // bg-green-600
+          'gray' : '#444941',
+          'green_pine' : '#1C6758',
+          'brown_light' : '#EFFCEF'
         },
       },
     },
