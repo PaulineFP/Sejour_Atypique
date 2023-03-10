ac8a7a2f059093f401491333b15f87ea1682c180
Pauline <pauline.frare-pradal@laposte.net>
2023-03-10 17:22:05 +0100

    :sparkles: Style: reservation form

--- public/js/modal.js
+++ public/js/modal.js
@@ -6,3 +6,22 @@ modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
 function toggleModal(){
     modalContainer.classList.toggle("active")
 }
+
+// Animation formulaire
+const inputs = document.querySelectorAll('input');
+
+for (let i = 0; i < inputs.length; i++) {
+  
+    let field = inputs[i];
+
+    field.addEventListener('input', (e) => {
+
+        if(e.target.value != ""){
+            e.target.parentNode.classList.add('animation');
+        } else if (e.target.value == "") {
+            e.target.parentNode.classList.remove('animation');
+        }
+
+    })
+
+}
