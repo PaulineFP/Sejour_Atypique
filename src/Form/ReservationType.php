e4952e55ad09f8b081e08efa095fa45e9ba40c76
Pauline <pauline.frare-pradal@laposte.net>
2023-04-20 19:12:33 +0200

    :computer: Ci : correct bug reservation form

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -4,10 +4,18 @@ namespace App\Form;
 
 use App\Entity\Reservations;
 use App\Entity\Users;
+use App\Entity\Hebergement;
 use Doctrine\ORM\Query\Expr\Func;
 use PhpParser\Builder\Function_;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,63 +24,27 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('reference')
-            ->add('created_at')
-            ->add('person_nb')
-            ->add('child_nb')
-            ->add('night_nb')
-            ->add('arrived')
-            ->add('price')
-            ->add('hebergement')
-            ->add('users', EntityType::class, array(
-                'class' => Users::class,
-                'choice_value' => function (?Users $users) {
-                    return $users ? $users->getId() : '';
-                }        
-                ) )
-            ->add('name', EntityType::class, array(
-                // Recherche les choix de l'entité 
-                'class' => Users::class,
-                // Permet d’intégrer/rechercher la bonne valeur au bonne endroit
-                'choice_value' => function (?Users $users) {
-                    return $users ? $users->getName() : '';
-                }, 
-                //Pour ajouter le champs qui ne fait pas partie de l'entité de base
-                'mapped' => false           
-                ) 
-            )
-            ->add('username', EntityType::class, array(
-                'class' => Users::class,
-                'choice_value' => function (?Users $users) {
-                    return $users ? $users->getUsername() : '';
-                }, 
-                'mapped' => false           
-                ) 
-            )
-            ->add('email', EntityType::class, array(
-                'class' => Users::class,
-                'choice_value' => function (?Users $users) {
-                    return $users ? $users->getEmail() : '';
-                }, 
-                'mapped' => false           
-                ) 
-            )
-            ->add('phone', EntityType::class, array(
-                'class' => Users::class,
-                'choice_value' => function (?Users $users) {
-                    return $users ? $users->getPhone() : '';
-                }, 
-                'mapped' => false           
-                ) 
-            )
-            ->add('reservations', EntityType::class, array(
-                'class' => Users::class,
-                'choice_value' => function (?Users $users) {
-                    return $users ? $users->getReservations() : '';
-                }, 
-                'mapped' => false           
-                ) 
-            )
+            ->add('reference', TypeTextType::class, ['by_reference' => true] )
+            ->add('created_at', DateType::class, ['by_reference' => true])
+            ->add('person_nb', NumberType::class)
+            ->add('child_nb', NumberType::class)
+            ->add('night_nb', NumberType::class)
+            ->add('arrived', DateType::class)
+            ->add('price', MoneyType::class)
+            ->add('hebergement', EntityType::class, [
+                'class' => Hebergement::class,
+                'choice_value' => function (?Hebergement $hebergement) {
+                    return $hebergement ? $hebergement->getId() : '';
+                }
+            ])
+            ->add(
+                $builder->create('users', FormType::class, ['by_reference' => false])
+                    ->add('name', TypeTextType::class)
+                    ->add('username', TypeTextType::class)
+                    ->add('email', EmailType::class)
+                    ->add('phone', TelType::class)
+                    ->add('reservations', TypeTextType::class, ['by_reference' => true])
+            )    
         ;
     }
 
@@ -80,8 +52,8 @@ class ReservationType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Reservations::class,
-            //Voir si c est bien ca si dessous
             'data_class' => Users::class,
+            'data_class' => Hebergement::class
         ]);
     }
 }
